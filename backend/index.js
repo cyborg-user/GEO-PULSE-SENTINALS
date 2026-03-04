d7ce15f9f4a8eb940673427f436ee898196b5609
shaurya shukla <shuklashaurya197@gmail.com>
2026-03-04 23:43:23 +0530

    https://github.com/cyborg-user/GEO-PULSE-SENTINALS

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("chitranjan 🚀");
+  res.send("server starting 🚀");
 });
 
 app.listen(5000, () => {
